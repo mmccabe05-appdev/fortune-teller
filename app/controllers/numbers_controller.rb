df72c1997199fff4cde9d66bf598cc78521b6f10
mmccabe05 <mmccabe@chicagobooth.edu>
2022-10-22 19:12:44 +0000

    Aries, Leo and Sagittarius all working

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -4,10 +4,21 @@ class NumbersController < ApplicationController
 
     5.times do
       giraffe = rand(1...100)
-      
+
       @zebra.push(giraffe)
     end
 
-    render({ :template => "lottery_stuff/woohoo.html.erb"})
-  end 
+    render({ :template => "lottery_stuff/woohoo.html.erb" })
+  end
+
+  def unlucky
+    @unlucky = Array.new
+
+    5.times do
+      unlucky_nums = rand(1...100)
+      @unlucky.push(unlucky_nums)
+    end
+
+    render({ :template => "lottery_stuff/unlucky.html.erb" })
+  end
 end
